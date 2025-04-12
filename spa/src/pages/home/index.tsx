860abbf8902e7fceb4fe46c91c648677c95e1297
Abdurashid <abdurashid.agzamov@esynergy.lv>
2025-04-12 14:12:30 +0500

    Text fixed

--- spa/src/pages/home/index.tsx
+++ spa/src/pages/home/index.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Home = () => {
-  return <div>dsdsadasdsdasd</div>;
+  return (
+    <div>
+      <h1>Docker Project</h1>
+    </div>
+  );
 };
 
 export default Home;
